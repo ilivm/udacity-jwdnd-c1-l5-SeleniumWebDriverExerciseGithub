7522455f25985384c4794254978b9096b659897e
ilivm <75082850+ilivm@users.noreply.github.com>
2024-09-24 17:16:07 +0200

    Initial File upload

---
+++ src/test/java/com/example/demo/SeleniumTest.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.List;
+
+public class SeleniumTest {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver = new ChromeDriver(options);
+
+//        WebDriver driver = new ChromeDriver();
+        driver.get("http://localhost:8080/animal");
+//        WebElement inputField = driver.findElement(By.name("q"));
+        WebElement inputField = driver.findElement(By.id("animalText"));
+        inputField.sendKeys("selenium");
+        inputField.submit();
+        List<WebElement> results = driver.findElements(By.cssSelector("div.g a"));
+        for (WebElement element : results) {
+            String link = element.getAttribute("href");
+            System.out.println(link);
+        }
+        Thread.sleep(5000);
+        driver.quit();
+
+    }
+}
